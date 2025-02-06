8be8fd389f922f9b34b18558a1e5f657f6fd07f0
paulworking123 <paulworking2420@gmail.com>
2025-02-06 23:40:01 +0700

    Update

--- apollo-client.tsx
+++ apollo-client.tsx
@@ -1,37 +1,3 @@
-// import {
-//   ApolloClient,
-//   InMemoryCache,
-//   HttpLink,
-//   ApolloLink,
-// } from "@apollo/client";
-// import { setContext } from "@apollo/client/link/context";
-// import Cookies from "js-cookie";
-
-// const createApolloClient = () => {
-//   const httpLink = new HttpLink({
-//     uri: "https://klyjbmqt8c.execute-api.ap-southeast-1.amazonaws.com/graphql",
-//   });
-
-//   const authLink = setContext((_, { headers }) => {
-//     const token = Cookies.get("auth_token");
-//     return {
-//       headers: {
-//         ...headers,
-//         Authorization: token ? token : "",
-//       },
-//     };
-//   });
-
-//   const link = ApolloLink.from([authLink, httpLink]);
-
-//   return new ApolloClient({
-//     link,
-//     cache: new InMemoryCache(),
-//   });
-// };
-
-// export default createApolloClient;
-
 import {
   ApolloClient,
   InMemoryCache,
@@ -43,7 +9,8 @@ import Cookies from "js-cookie";
 
 const createApolloClient = () => {
   const httpLink = new HttpLink({
-    uri: "https://klyjbmqt8c.execute-api.ap-southeast-1.amazonaws.com/graphql",
+    // uri: "https://klyjbmqt8c.execute-api.ap-southeast-1.amazonaws.com/graphql",
+    uri: "https://api.tiktokshop.online/graphql",
   });
 
   const authLink = setContext((_, { headers }) => {
