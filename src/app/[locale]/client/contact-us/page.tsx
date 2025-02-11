6731b62b36695696e2a3be4f582d3ff58b41376b
Paokue Saolong <paokue@Paokues-MacBook-Pro.local>
2025-02-11 22:04:20 +0700

    Update contact page

--- src/app/[locale]/client/contact-us/page.tsx
+++ src/app/[locale]/client/contact-us/page.tsx
@@ -1,13 +1,15 @@
 import Breadcrumb from "@/components/breadCrumb";
 import { TelegramIcon } from "@/icons/page";
+import { useTranslations } from "next-intl";
 
 export default function ContactUs() {
+  const t = useTranslations("contact_page");
   return (
     <>
       <Breadcrumb
         items={[
-          { label: "Dashboard", value: "/client" },
-          { label: "Contact us", value: "/contact-us" },
+          { label: t("_dashboard"), value: "/client" },
+          { label: t("_contact_us"), value: "/contact-us" },
         ]}
       />
       <a
@@ -16,17 +18,14 @@ export default function ContactUs() {
         rel="noopener noreferrer"
         className="flex items-center justify-center flex-col gap-3 rounded py-6 text-gray-500 mt-6 cursor-pointer"
       >
-        <h3 className="font-large text-black">Contact Us</h3>
+        <h3 className="font-large text-black">{t("_contact_us")}</h3>
         <div className="rounded-md bg-white flex items-start justify-between p-6">
           <div className="bg-blue-500 p-3 rounded-full">
             <TelegramIcon size={28} className="text-white" />
           </div>
           <div className="flex flex-col items-center justify-center ml-2 gap-4">
             <p className="text-md text-black">Telegram</p>
-            <p className="text-xs">
-              Reach out to our customer service team for assistance with any
-              issues.
-            </p>
+            <p className="text-xs">{t("_description")}</p>
           </div>
         </div>
       </a>
