f449c082dd81db5f65c719412ccd0499789ebe09
Paokue Saolong <paokue@Paokues-MacBook-Pro.local>
2025-03-01 15:28:07 +0700

    Update

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -6,11 +6,60 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import { ReduxProvider } from "@/redux/provider";
 import { NextIntlClientProvider } from "next-intl";
+import DevToolsRedirect from "@/components/devtoolDetector";
 
 export const metadata: Metadata = {
-  title: "tiktokshop.net",
-  description: "Tiktok shop is the largest and biggest e-commerce platform",
-  keywords: ["tiktokshop", "shopping.tiktok", "online shopping"],
+  title: "TiktokShop - The Ultimate Online Shopping Destination",
+  description:
+    "Discover the best deals on TiktokShop, the largest e-commerce platform. Shop trending products, exclusive discounts, and fast delivery worldwide.",
+  keywords: [
+    "TiktokShop",
+    "Tiktok shopping",
+    "online shopping",
+    "buy online",
+    "e-commerce",
+    "best deals",
+    "trending products",
+    "discounts",
+    "fast delivery",
+    "secure shopping",
+  ],
+  openGraph: {
+    title: "TiktokShop - The Ultimate Online Shopping Destination",
+    description:
+      "Shop the latest trending products with exclusive deals and fast delivery on TiktokShop.",
+    url: "https://tiktokshop.online",
+    siteName: "TiktokShop",
+    images: [
+      {
+        url: "https://res.cloudinary.com/dvh8zf1nm/image/upload/v1740281086/logo2_dwqmfv.png",
+        width: 1200,
+        height: 630,
+        alt: "TiktokShop - Online Shopping",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "TiktokShop - The Ultimate Online Shopping Destination",
+    description:
+      "Shop trending products, get exclusive discounts, and enjoy fast delivery worldwide on TiktokShop.",
+    images: [
+      "https://res.cloudinary.com/dvh8zf1nm/image/upload/v1740281086/logo2_dwqmfv.png",
+    ],
+  },
+  robots: {
+    index: true, // Allow search engines to index the page
+    follow: true, // Allow search engines to follow links on the page
+  },
+  alternates: {
+    canonical: "https://tiktokshop.online", // Canonical URL to prevent duplicate content issues
+  },
+  authors: [
+    { name: "TiktokShop Team", url: "https://tiktokshop.online/about" },
+  ],
+  publisher: "TiktokShop Online",
 };
 
 interface RootLayoutProps {
@@ -36,6 +85,7 @@ export default async function RootLayout({
       <DynamicApolloClientWrapper>
         <html lang={locale}>
           <body className="bg-dark font-sans">
+            <DevToolsRedirect />
             <div className="justify-center text-white h-screen">
               <ReduxProvider>{children}</ReduxProvider>
             </div>
